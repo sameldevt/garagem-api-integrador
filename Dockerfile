3647bd85bcdcc774f54b20e097b21a197394cc5d
Michelle <mmorilhas@almavivasolutions.com.br>
2024-08-26 15:48:34 -0300

    chore: adicionando dev e test properties e alterando Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,6 @@ COPY target/garagem.integrador.redhat-0.0.1-SNAPSHOT.jar app.jar
 
 ENTRYPOINT ["java", "-jar", "app.jar"]
 
+CMD ["--spring.config.location=file:/config/secret.properties"]
+
 EXPOSE 8081
--- src/test/java/br/com/almavivasolution/integrador/IntegradorApplicationTests.java
+++ src/test/java/br/com/almavivasolution/integrador/IntegradorApplicationTests.java
@@ -2,8 +2,12 @@ package br.com.almavivasolution.integrador;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import br.com.almavivasolutions.integrador.IntegradorApplication;
+
+@SpringBootTest(classes = IntegradorApplication.class)
+@ActiveProfiles("test")
 class IntegradorApplicationTests {
 
 	@Test
